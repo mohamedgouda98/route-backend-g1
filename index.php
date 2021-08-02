706e447cb283b81c138338e886a158744717716c
macbookpro <dev.mohamedgouda@gmail.com>
2021-08-02 21:54:45 +0200

    init project from lec5

---
+++ index.php
@@ -0,0 +1,37 @@
+<?php
+include 'base/navbar.php';
+?>
+
+<h5>Index Page</h5>
+
+<form method="post" action="">
+    <input type="email" required name="email" placeholder="Email" id=""><br><br>
+    <input type="number" required name="salary" placeholder="number" id=""><br><br>
+    <button name="submit">submit</button>
+
+</form>
+
+
+
+<?php
+
+if(isset($_POST['submit']))
+{
+   $email = $_POST['email'];
+   $salary = $_POST['salary'];
+
+}
+
+?>
+
+
+<div class="info">
+    <h3> Email is : <?php echo $email?> </h3>
+    <h5> Salary is : <?php echo $salary?> </h5>
+
+</div>
+
+<?php
+    include 'base/fotter.php';
+?>
+
