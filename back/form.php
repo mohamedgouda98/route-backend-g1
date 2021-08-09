b8df6dc841b4a3f7376b586ba9ea1734f26273d5
macbookpro <dev.mohamedgouda@gmail.com>
2021-08-09 20:45:05 +0200

    login v0.1

--- back/form.php
+++ back/form.php
@@ -2,85 +2,21 @@
 
 if(isset($_POST['submit']))
 {
+   $user = [$_POST['email'], $_POST['password']];
 
-    $numbers = [
-        $_POST['deg1'],
-        $_POST['deg2'],
-        $_POST['deg3'],
-        $_POST['deg4'],
-        $_POST['deg5'],
+    $data = [
+        ['gouda@gmail.com', "123456"],
+        ['test@gmail.com',"12345678"],
+        ['ahmed@cloud.com',"246810"]
     ];
 
-
-    function studentSuDeg($numbers)
+    if(in_array($user, $data))
     {
-        $array = [];
-        for($i=0; $i<count($numbers); $i++)
-        {
-            if($numbers[$i] >= 50)
-            {
-                $array[] = $numbers[$i];
-            }
-        }
-
-        return $array;
-
+        echo "yes";
+    }else{
+        echo "no";
     }
 
 
-    function studentFaDeg($numbers)
-    {
-        $array = [];
-        for($i=0; $i<count($numbers); $i++)
-        {
-            if($numbers[$i] < 50)
-            {
-                $array[] = $numbers[$i];
-            }
-        }
-        return $array;
-    }
-
-    $suNumbers= studentSuDeg($numbers);
-    $faNumbers= studentFaDeg($numbers);
 
 }
-
-
-?>
-
-
-
-<table>
-    <tr>
-        <th>Deg</th>
-        <th>Status</th>
-    </tr>
-
-    <?php
-        foreach ($suNumbers as $number)
-        {
-           ?>
-            <tr>
-                <td><?php echo $number?></td>
-                <td>Su</td>
-            </tr>
-        <?php
-        }
-
-        foreach ($faNumbers as $number)
-        {
-           ?>
-            <tr>
-                <td><?php echo $number?></td>
-                <td>Fa</td>
-            </tr>
-        <?php
-        }
-
-    ?>
-
-
-
-</table>
-
--- index.php
+++ index.php
@@ -4,14 +4,10 @@ include 'base/navbar.php';
 ?>
 
 <h5>Index Page</h5>
-<!---->
 
 <form method="post" action="back/form.php">
-    <input type="number" required name="deg1" placeholder="deg1" id=""><br><br>
-    <input type="number" required name="deg2" placeholder="deg2" id=""><br><br>
-    <input type="number" required name="deg3" placeholder="deg3" id=""><br><br>
-    <input type="number" required name="deg4" placeholder="deg4" id=""><br><br>
-    <input type="number" required name="deg5" placeholder="deg5" id=""><br><br>
+    <input type="email" required name="email" placeholder="email" id=""><br><br>
+    <input type="password" required name="password" placeholder="password" id=""><br><br>
     <button name="submit">submit</button>
 </form>
 
