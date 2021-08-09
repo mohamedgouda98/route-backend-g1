ba4a39d2d243d4853d4665fe180c70dab6272d47
macbookpro <dev.mohamedgouda@gmail.com>
2021-08-09 19:18:08 +0200

    soul 1

--- back/form.php
+++ back/form.php
@@ -2,20 +2,93 @@
 
 if(isset($_POST['submit']))
 {
-    $numbers = ["01000000", "01111111", "012222222"];
+    $deg1 = $_POST['deg1'];
+    $deg2 = $_POST['deg2'];
+    $deg3 = $_POST['deg3'];
+    $deg4 = $_POST['deg4'];
+    $deg5 = $_POST['deg5'];
 
-    $firstName = $_POST['firstName'];
-    $lastName = $_POST['lastName'];
-    $phone = $_POST['phone'];
+    $suNumbers= [];
+    $faNumbers= [];
 
-    if(array_search($phone, $numbers))
+    if($deg1>= 50)
     {
-        echo "yes";
-    }else{
-        echo "invalid Number";
+        $suNumbers[] = $deg1;
+    }else
+    {
+        $faNumbers[] = $deg1;
     }
 
-//    echo $firstName . '<br>';
-//    echo $phone;
+    if($deg2>= 50)
+    {
+        $suNumbers[] = $deg2;
+    }else
+    {
+        $faNumbers[] = $deg2;
+    }
+
+    if($deg3>= 50)
+    {
+        $suNumbers[] = $deg3;
+    }else
+    {
+        $faNumbers[] = $deg3;
+    }
+
+    if($deg4>= 50)
+    {
+        $suNumbers[] = $deg4;
+    }else
+    {
+        $faNumbers[] = $deg4;
+    }
+
+    if($deg5>= 50)
+    {
+        $suNumbers[] = $deg5;
+    }else
+    {
+        $faNumbers[] = $deg5;
+    }
+
 
 }
+
+
+?>
+
+
+
+<table>
+    <tr>
+        <th>Deg</th>
+        <th>Status</th>
+    </tr>
+
+    <?php
+        foreach ($suNumbers as $number)
+        {
+           ?>
+            <tr>
+                <td><?php echo $number?></td>
+                <td>Su</td>
+            </tr>
+        <?php
+        }
+
+        foreach ($faNumbers as $number)
+        {
+           ?>
+            <tr>
+                <td><?php echo $number?></td>
+                <td>Fa</td>
+            </tr>
+        <?php
+        }
+
+    ?>
+
+
+
+</table>
+
--- index.php
+++ index.php
@@ -5,29 +5,17 @@ include 'base/navbar.php';
 
 <h5>Index Page</h5>
 <!---->
-<!--<form method="post" action="back/form.php">-->
-<!--    <input type="text" required name="firstName" placeholder="firstName" id=""><br><br>-->
-<!--    <input type="text" required name="lastName" placeholder="lastName" id=""><br><br>-->
-<!--    <input type="number" required name="phone" placeholder="phone" id=""><br><br>-->
-<!--    <button name="submit">submit</button>-->
-<!--</form>-->
+
+<form method="post" action="back/form.php">
+    <input type="number" required name="deg1" placeholder="deg1" id=""><br><br>
+    <input type="number" required name="deg2" placeholder="deg2" id=""><br><br>
+    <input type="number" required name="deg3" placeholder="deg3" id=""><br><br>
+    <input type="number" required name="deg4" placeholder="deg4" id=""><br><br>
+    <input type="number" required name="deg5" placeholder="deg5" id=""><br><br>
+    <button name="submit">submit</button>
+</form>
 
 
-<?php
-
-
-echo "name" . '<br>';
-echo "name" . '<br>';
-echo "name" . '<br>';
-echo "name" . '<br>';
-echo "name" . '<br>';
-echo "name" . '<br>';
-echo "name" . '<br>';
-
-
-
-
-?>
 
 
 
