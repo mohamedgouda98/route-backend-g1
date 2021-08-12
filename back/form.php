b0d66ed581d3935fedf37921924ea26bfcd7edd0
macbookpro <dev.mohamedgouda@gmail.com>
2021-08-12 21:47:34 +0200

    commit 2

--- back/form.php
+++ back/form.php
@@ -1,87 +1,51 @@
 <?php
-//
-//class auth{
-//
-//    public $data = [
-//                ['gouda@gmail.com', "123456"],
-//                ['test@gmail.com',"12345678"],
-//                ['ahmed@cloud.com',"246810"]
-//            ];
-//
-//    public $userDataInClass;
-//
-//    public function login()
-//    {
-//       if(in_array($this->userDataInClass, $this->data))
-//       {
-//           return true;
-//       }
-//
-//    }
-//
-//
-//}
-//
-//
-//
-//if(isset($_POST['submit']))
-//{
-//    $userDataFromUser = [$_POST['email'], $_POST['password']];
-//
-//    $user = new auth();
-//    $user->userDataInClass = $userDataFromUser;
-//
-//
-//    echo $user->login();
-//}
-//
-//
 
-
-
-
-
-
-class car
+interface Auth
 {
-    public $color;
-    public $size;
-
-    public function __construct($color, $size)
-    {
-        $this->color = $color;
-        $this->size = $size;
-    }
-
-    public function test($number, $tax)
-    {
-        return $number + $tax ;
-    }
-
-}
-
-class bmw extends car
-{
-    public $model;
-
-    public function __construct($color, $size, $model)
-    {
-        $this->model = $model;
-        parent::__construct($color, $size);
-    }
-
-    public function echoFunctionTest()
-    {
-       echo $this->color;
-    }
-
-
+    public function login();
 }
 
 
-$newCar = new bmw("black", "4", "2022");
-echo $newCar->model;
-//$newCar->echoFunctionTest();
+class userAuth implements Auth
+{
+    private $email;
+    private $password;
+    private $userData;
+
+    private $emails = [
+        ['dev.mohamedgouda@gmail.com', '12345678'],
+        ['dev@gmail.com', '12345678'],
+        ['mohamed@gmail.com', '12345678'],
+    ];
+
+    public function __construct($email, $password)
+    {
+        $this->email = $email;
+        $this->password = $password;
+        $this->userData = [$email, $password];
+    }
+
+    public function login(){
+        if(in_array($this->userData, $this->emails))
+        {
+            echo "Auth User";
+        }else{
+            echo "User Not Found";
+        }
+
+    }
+}
 
 
 
+
+
+if(isset($_POST['submit']))
+{
+    $email = $_POST['email'];
+    $password = $_POST['password'];
+
+    $user = new userAuth($email, $password);
+    $user->login();
+
+}
